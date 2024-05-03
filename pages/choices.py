70ebede0bd1bbcc078e5211c1d4dd284ba18f925
Brakister <smashbored@gmail.com>
2024-05-03 05:39:59 -0300

    db basica

--- pages/choices.py
+++ pages/choices.py
@@ -1,18 +1,18 @@
 TMO_CHOICES = [
-    ('STLAVSIM', 'Lavagem Simples', 34.98, 51.00),
-    ('STPREPS','Preparação de Seminovos',181.50, 181.50 ),
-    ('STLAVCRISTAL','Lavagem com Cera + Cristalização para Brisa',58.30, 115.40),
-    ('STKITPLUS', 'Kit Plus - Kit3x1 - Polimento + Selante + Higienização Interna + Hidratação de Banco + Cristalização de Vidros',413.93, 761.25),
-    ('STLAVMOT', 'Lavagem com Motor', 37.90, 69.30),
-    ('STPREPN', 'Preparação de Novos',121.00,121.00),
-    ('STHIGIE', 'Higienização de Bancos e Teto',250.69, 424.20),
-    ('STCERAMIC9H', 'Vitrificação de Pintura 9H ou 10H (Polimento + Cristalização + 9H ou 10H)',771.89, 1346.10),
-    ('STREMOP','Remoção de Pelicula', 18.15, 18.15),
-    ('STCRIV', 'Serv. Terc. - Cristalização de vidros',72.29, 164.40),
-    ('STHIDBA', 'Serv. Terc. - Hidratação dos bancos', 247.19, 421.05),
-    ('STKIT31', 'Serv. Terc. - KIT3x1 - Polimento + Selante + Higienização interna + hidratação de banco',355.63, 656.25),
-    ('STKITION', 'Serv. Terc. - Polimento + Selante', 329.98, 597.45),
-    ('STVITRI', 'Vitrificação de Pintura 5h (polimento + Cristaçiza + 5h)',565.51, 1002.75)
+    ('STLAVSIM', 'Lavagem Simples'), # 34.98, 51.00),
+    ('STPREPS','Preparação de Seminovos'), # 181.50, 181.50 ),
+    ('STLAVCRISTAL','Lavagem com Cera + Cristalização para Brisa'), # ,58.30, 115.40),
+    ('STKITPLUS', 'Kit Plus - Kit3x1 - Polimento + Selante + Higienização Interna + Hidratação de Banco + Cristalização de Vidros'), # ,413.93, 761.25),
+    ('STLAVMOT', 'Lavagem com Motor'), # , 37.90, 69.30),
+    ('STPREPN', 'Preparação de Novos'), # ,121.00,121.00),
+    ('STHIGIE', 'Higienização de Bancos e Teto'), # ,250.69, 424.20),
+    ('STCERAMIC9H', 'Vitrificação de Pintura 9H ou 10H (Polimento + Cristalização + 9H ou 10H)'), # ,771.89, 1346.10),
+    ('STREMOP','Remoção de Pelicula'), #, 18.15, 18.15),
+    ('STCRIV', 'Serv. Terc. - Cristalização de vidros'), # ,72.29, 164.40),
+    ('STHIDBA', 'Serv. Terc. - Hidratação dos bancos'), # , 247.19, 421.05),
+    ('STKIT31', 'Serv. Terc. - KIT3x1 - Polimento + Selante + Higienização interna + hidratação de banco'), # ,355.63, 656.25),
+    ('STKITION', 'Serv. Terc. - Polimento + Selante'), # , 329.98, 597.45),
+    ('STVITRI', 'Vitrificação de Pintura 5h (polimento + Cristaçiza + 5h)'), # ,565.51, 1002.75)
     ]
 
 DEALER_CHOICES = [
--- pages/models.py
+++ pages/models.py
@@ -1,50 +1,21 @@
 from django.db import models
-from datetime import date
-from .choices import TMO_CHOICES , DEALER_CHOICES
+from .choices import TMO_CHOICES, DEALER_CHOICES
 
-
-
-
-class Servico:
-
-    def __init__(self, tmo):
-        self.TMO=tmo
-        self.DescServ = None
-        self.CustoUnit = None
-        self.ValorCAOA = None
-    
-    
-    def buscar_dados(self):
-        for codigo_tabela, descricao, custo, valor in TMO_CHOICES:
-            if self.TMO == codigo_tabela:
-                self.DescServ = descricao
-                self.CustoUnit = custo
-                self.ValorCAOA = valor
-                break
+class Servico(models.Model):
+    TMO = models.CharField(max_length=50, choices=TMO_CHOICES)
+    DescServ = models.CharField(max_length=255)
+    CustoUnit = models.DecimalField(max_digits=10, decimal_places=2)
+    ValorCAOA = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.DescServ
 
-    def printServ(self):
-        print(self.TMO, self.DescServ, self.CustoUnit, self.ValorCAOA)
-
-class Loja:
-
-    def __init__ (self, dealer):
-        self.DEALER = dealer
-        self.NomeLoja = None
-    
-    def buscar_dados(self):#
-        for codigo_tabela, nome_loja in DEALER_CHOICES:
-            if self.DEALER == codigo_tabela:
-                self.NomeLoja = nome_loja
-                break
-    
-    
+class Loja(models.Model):
+    DEALER = models.CharField(max_length=50, choices=DEALER_CHOICES)
+    NomeLoja = models.CharField(max_length=255)
 
     def __str__(self):
         return self.NomeLoja
-
 
 class OrdemDeServico(models.Model):
     numero_os = models.CharField(max_length=50)
@@ -52,15 +23,5 @@ class OrdemDeServico(models.Model):
     descricao_pedido = models.TextField()
     tmos_selecionados = models.JSONField()
 
-    def adicionar_tmo(self, tmo):
-        # Aqui você pode chamar a função da classe Servico para buscar os dados do TMO e adicionar à ordem de serviço
-        servico = Servico.buscar_dados(tmo)
-        # Lógica para adicionar o TMO à ordem de serviço
-        self.tmos.add(servico)
-
-
-    
     def __str__(self):
-        return f"OS-{self.numero_os} ({self.TMOS})"
-
-
+        return f"OS-{self.numero_os}"
