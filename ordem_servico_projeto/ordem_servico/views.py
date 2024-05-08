7ee8c93aa4f323e2e0c56bba44174cf23a906308
Brakister <smashbored@gmail.com>
2024-05-08 09:15:01 -0300

    Pseudo finalização do back-end

--- ordem_servico_projeto/ordem_servico/views.py
+++ ordem_servico_projeto/ordem_servico/views.py
@@ -32,12 +32,31 @@ def baixar_excel(request, mes):
     wb = Workbook()
     ws = wb.active 
     ws.append(['Empresa', 'Serviço', 'Produto', 'Data'])
+
+    
+    ordens_servico = EmissaoOrdemServico.objects.filter(data__month=mes)
+    for ordem in ordens_servico:
+        
+        data_sem_fuso_horario = ordem.data.replace(tzinfo=None)
+        ws.append([ordem.empresa, ordem.servico, ordem.produto, data_sem_fuso_horario])
+
+    
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    
+    response['Content-Disposition'] = f'attachment; filename=ordens_servico_{mes}.xlsx'
+    
+    wb.save(response)
+
+    return response
+
+    return response
+
 def historico_ordem_servico(request, mes=None):
     if mes and 1 <= 12:
         ordens_servico = EmissaoOrdemServico.objects.filter(data__month=mes)
     else:
         ordens_servico = EmissaoOrdemServico.objects.all()
-        mes = 1 # Define um valor padrão para 'mes'
+        mes = 1 
         ordens_servico = EmissaoOrdemServico.objects.filter(data__month=mes)
     return render(request, 'ordem_servico/historico_ordem_servico.html', {'ordens_servico': ordens_servico, 'mes': mes})
    
@@ -52,18 +71,17 @@ def historico_ordem_servico(request, mes=None):
 
 
 def emitir_planilha(request, mes, ano):
-    # Aqui você pode adicionar a lógica para gerar a planilha com base nos parâmetros mes e ano
-    # Por exemplo, você pode filtrar os objetos EmissaoOrdemServico com base nesses parâmetros
+    
     ordens_servico = EmissaoOrdemServico.objects.filter(data__month=mes, data__year=ano)
 
 
 
-    # Gere a planilha (este é apenas um exemplo, você precisará adaptar de acordo com suas necessidades)
+   
     planilha = "Planilha de Ordens de Serviço para o mês {} do ano {}\n".format(mes, ano)
     for ordem in ordens_servico:
         planilha += "Empresa: {}, Serviço: {}, Data: {}\n".format(ordem.empresa, ordem.servico, ordem.data)
 
-    # Retorne a planilha como uma resposta HTTP
+    
     return HttpResponse(planilha, content_type='text/plain')
     return response
 
