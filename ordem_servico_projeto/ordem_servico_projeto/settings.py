ecc4c6bbbb1223819cb075e0a13423c35eb50310
Douglas Kugler <2231291@aluno.univesp.br>
2024-05-15 19:55:13 -0300

    Adição do css para o front-end

--- ordem_servico_projeto/ordem_servico_projeto/settings.py
+++ ordem_servico_projeto/ordem_servico_projeto/settings.py
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+
+# Diretórios adicionais para arquivos estáticos (opcional, se necessário)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
